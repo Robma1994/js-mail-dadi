8ee0a8f63fc1141a3bc4447cdefdfa4b1e8e3ff1
Roberto <robertomartinonwg@gmail.com>
2021-07-18 12:33:54 +0200

    Esercizi completati con successo.

--- js/main.js
+++ js/main.js
@@ -16,3 +16,33 @@ if (numeroGiocatore > numeroComputer) {
 } else {
     console.log("Pareggio")
 }
+
+//Mail:
+
+
+
+
+// 1 - Crea un database di e-mail.
+
+var emailDatabase = ["Giovanni", "Antonio", "Paolo"];
+
+// 2 - Chiedi all’utente la sua email.
+var richiestaEmail = prompt("Inserisci la tua email")
+
+// 3 - Controlla che sia nella lista di chi può accedere.
+
+for (i=0; i < emailDatabase.length; i++) {
+    if (richiestaEmail == emailDatabase[i]) {
+        document.getElementById("email").innerHTML = "Il tuo nome è già presente nel nostro Database"
+        console.log("Il tuo nome è gia presente nel nostro Database");
+    } else if (richiestaEmail !== emailDatabase[1]) {
+        document.getElementById("email").innerHTML = "Puoi aggiungere la tua email"
+        console.log("Puoi aggiungere la tua email")
+    }
+} 
+
+
+// 4 - Se non può accedere invia un messaggio.
+
+
+
